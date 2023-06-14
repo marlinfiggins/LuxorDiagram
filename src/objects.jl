9e597a6ea482e5618b4c32e12ac57806cc179894
marlinfiggins <marlinfiggins@gmail.com>
2023-06-14 13:11:36 -0700

    Formatting

--- src/objects.jl
+++ src/objects.jl
@@ -1,9 +1,8 @@
-
-struct Compartment{T<:Real, S<:AbstractString}
+struct Compartment{T<:Real,S<:AbstractString}
     pos::Point
     width::T
     height::T
-    text::Union{S, Nothing}
+    text::Union{S,Nothing}
 end
 
 function Compartment(pos, width, height)
@@ -18,7 +17,7 @@ function draw(c::Compartment; color="white", rt=0.5)
     squircle(c.pos, c.width, c.height; action=:stroke, rt=rt)
 
     if !isnothing(c.text)
-        text(c.text, c.pos, halign=:center, valign=:middle)
+        text(c.text, c.pos; halign=:center, valign=:middle)
     end
 end
 
@@ -27,7 +26,7 @@ end
 struct Transition
     from::Compartment
     to::Compartment
-    text::Union{S, Nothing} where {S <: AbstractString}
+    text::Union{S,Nothing} where {S<:AbstractString}
 end
 
 function Transition(from, to)
@@ -41,10 +40,10 @@ function squricleedges(t::Transition; scale_radius=0.8)
     p_radius = sqrt(t.from.width^2 + t.from.height^2) * scale_radius
 
     c_pos = t.to.pos
-    c_radius =  sqrt(t.to.width^2 + t.to.height^2)  * scale_radius
+    c_radius = sqrt(t.to.width^2 + t.to.height^2) * scale_radius
 
     # Angle between two points
-    θ = atan(p_pos.y-c_pos.y, p_pos.x-c_pos.x)
+    θ = atan(p_pos.y - c_pos.y, p_pos.x - c_pos.x)
 
     # Origin and Destination of arrow
     orig = Point(p_pos.x - p_radius * cos(θ), p_pos.y - p_radius * sin(θ))
@@ -52,16 +51,13 @@ function squricleedges(t::Transition; scale_radius=0.8)
     return orig, dest
 end
 
-
 function arrowtext(t)
-    text(t, Point(0,0) + Point(0, -5), halign=:center, valign=:bottom)
+    return text(t, Point(0, 0) + Point(0, -5); halign=:center, valign=:bottom)
 end
 
 # Add option for text over arrow
 function draw(t::Transition; linewidth=2.5, scale_radius=0.8)
-    text = isnothing(t.text) ? "" : t.text 
-    orig, dest = squricleedges(t, scale_radius=scale_radius)
-    arrow(orig, dest, 
-        linewidth=linewidth, 
-        decorate = () -> arrowtext(text))
+    text = isnothing(t.text) ? "" : t.text
+    orig, dest = squricleedges(t; scale_radius=scale_radius)
+    return arrow(orig, dest; linewidth=linewidth, decorate=() -> arrowtext(text))
 end
